4a52a73bf32c599700e1beec41bf147e9b6fb3f1
Kethan3 <kethangowda003@gmail.com>
2025-03-10 15:38:36 +0530

    feat: enable calculator functionality in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-// import calculator from './calculator';
+import calculator from './calculator';
 
 
 
-// let calc = new calculator();
+let calc = new calculator();
 
-// console.log(calc.add(2, 4));
-// console.log(calc.subtract(2, 4));
-// console.log(calc.multiply(2, 4));
-// console.log(calc.divide(2, 4));
+console.log(calc.add(2, 4));
+console.log(calc.subtract(2, 4));
+console.log(calc.multiply(2, 4));
+console.log(calc.divide(2, 4));
 
 
